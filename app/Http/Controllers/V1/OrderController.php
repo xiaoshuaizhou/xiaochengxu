e88d9a26dad5b80aff8d4db542220fc7d750e1f8
zhouxiaoshuai <zhouxiaoshuai3@gmail.com>
2018-04-06 17:36:31 +0800

    处理订单流程

---
+++ app/Http/Controllers/V1/OrderController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+/**
+ *  订单接口
+ */
+class OrderController extends Controller
+{
+
+}
